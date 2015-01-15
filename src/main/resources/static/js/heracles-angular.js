e0f51132dc5bdf65caf765a0af86af18df18baa7
Charity Hilton <cahilton@regenstrief.org>
2015-01-15 11:48:27 -0500

    get the auto filter checkbox list thing going

--- src/main/resources/static/js/heracles-angular.js
+++ src/main/resources/static/js/heracles-angular.js
@@ -4,7 +4,22 @@
     	$scope.showCohort = function(datum) {
   		  $http.get('/data/sample-cohort-explorer.json')
   	       .then(function(res){
-  	          $scope.cohort = res.data;                
+  	    	   res.data.completed_cohorts = {};
+  	    	   res.data.new_cohorts = {};
+  	    	   $.each(res.data.analyses, function() {
+  	    		   if (this.done === true) {
+  	    			   if (!res.data.completed_cohorts[this.category]) {
+  	    				 res.data.completed_cohorts[this.category] = [];
+  	    			   }
+  	    			   res.data.completed_cohorts[this.category].push(this);
+  	    		   } else {
+  	    			   if (!res.data.new_cohorts[this.category]) {
+	    				 res.data.new_cohorts[this.category] = [];
+	    			   }
+	    			   res.data.new_cohorts[this.category].push(this);
+  	    		   }
+  	    	   });
+  	    	   $scope.cohort = res.data;                
   	        });
     	}
     });
--- src/main/resources/static/js/heracles-main.js
+++ src/main/resources/static/js/heracles-main.js
@@ -1,3 +1,63 @@
+jQuery.fn.filterByText = function(textbox, selectSingleMatch) {
+    return this.each(function() {
+        var select = this;
+
+        $(textbox).bind('change keyup', function() {
+            //var options = $(select).empty().data('options');
+            var search = $(this).val().trim();
+            var regex = new RegExp(search, "gi");
+            var shown = [];
+            $(select).find("label").each(function() {
+                // child = <label><input type='checkbox .../></label>
+                var child = $(this);
+                /*
+                if (child.hasClass("toggle-parent-label")) {
+                	return;
+                }
+                */
+                var childText = child.find("input").attr("value");
+                if (childText.match(regex) !== null) {
+                    child.show();
+                    var parent = child.find("input").attr("parent");
+                    if (parent && shown.indexOf(parent) < 0) {
+                        shown.push(parent);
+                    }
+                } else {
+                    child.hide();
+                }
+            });
+            // make sure parents are shown even if they text doesn't match
+            $.each(shown, function() {
+                $(".toggle-parent-label[key='" + this + "']").show();
+            });
+        });
+    });
+};
+jQuery.fn.multiselect = function() {
+    $(this).each(function() {
+        var checkboxes = $(this).find(".toggle-checkbox-item");
+        checkboxes.each(function() {
+            var checkbox = $(this);
+            // Highlight pre-selected checkboxes
+            if (checkbox.attr("checked")) checkbox.parent().addClass("multiselect-on");
+            // Highlight checkboxes that the user selects
+            checkbox.click(function() {
+                if (checkbox.attr("checked")) checkbox.parent().addClass("multiselect-on");
+                else checkbox.parent().removeClass("multiselect-on");
+            });
+        });
+    });
+};
+
+function toggleVisibleReports(checked, selector) {
+    $(selector).find("input[type='checkbox']:visible").prop('checked', checked);
+}
+
+// regular functions
+function split(val) {
+    return val.split(/,\s*/);
+}
+
 $(document).ready(function() {
 
 	// initialize the cohort type ahead, constructs the suggestion engine
@@ -56,6 +116,34 @@ $(document).ready(function() {
             }
         });
     });
+    
+    // set up auto checkbox filters
+    // init multiselect w/ checkboxes
+    $(function() {
+        $(".multiselect").multiselect();
+    });
+    // init filters
+    $(function() {
+    	$(".multiselect").each(function() {
+    		var filterKey = $(this).attr("filter-key");
+    		$(this).filterByText($(".auto-filter-check-list-input[filter-key='" + filterKey + "']"), true);
+    	});
+    });
+    //setup select/clear filters events
+    $(".auto-filter-check-list-select").click(function() {
+        toggleVisibleReports(true, ".multiselect[filter-key='" + $(this).attr("filter-key") + "']");
+        return false;
+    });
+    $(".auto-filter-check-list-clear").click(function() {
+        toggleVisibleReports(false, ".multiselect[filter-key='" + $(this).attr("filter-key") + "']");
+        return false;
+    });
+    // parents check and uncheck children
+    $(".auto-filter-check-list-parent-box").change(function() {
+        var checked = $(this).prop("checked");
+        $("input[parent='" + $(this).val() + "']:visible").prop("checked", checked);
+    });
+
 
     // focus on input box
     setTimeout(function(){

fed11cc57bac476f05dbe02283a6f1a40ed2361f
Charity Hilton <cahilton@regenstrief.org>
2015-01-26 16:26:43 -0500

    model pom after ohdsi setup

--- src/main/java/org/ohdsi/heracles/HeraclesApp.java
+++ src/main/java/org/ohdsi/heracles/HeraclesApp.java
@@ -1,17 +1,18 @@
 package org.ohdsi.heracles;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.context.web.SpringBootServletInitializer;
 
-@Configuration
-@ComponentScan
-@EnableAutoConfiguration
-public class HeraclesApp {
-
-    public static void main(String[] args) {
-        SpringApplication.run(HeraclesApp.class, args);
+@SpringBootApplication
+public class HeraclesApp extends SpringBootServletInitializer {
+    
+    @Override
+    protected SpringApplicationBuilder configure(final SpringApplicationBuilder application) {
+        return application.sources(HeraclesApp.class);
     }
-
+    
+    public static void main(final String[] args) throws Exception {
+        new SpringApplicationBuilder(HeraclesApp.class).run(args);
+    }
 }
